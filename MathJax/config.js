5d318de9c127946fc2b8447aeaa44e097ef0712e
Reiya <63145634+reiya0104@users.noreply.github.com>
2020-06-14 23:18:26 +0900

    Add files via upload

---
+++ MathJax/config.js
@@ -0,0 +1,16 @@
+// This is a JavaScript file named "config.js"
+window.MathJax = {
+    TeX: {
+        equationNumbers: {autoNumber: "AMS"},
+        Macros: {
+            x: "{\\times}",
+            bm: ["{\\boldsymbol{#1}}",1],
+            dd: ["{\\frac{\\partial #1}{\\partial #2}}",2],
+            mat: ["{\\begin{pmatrix} #1 & #2 \\\\ #3 & #4 \\end{pmatrix}}",4]
+        }
+    },
+    CommonHTML: {
+        scale: 90,
+        mtextFontInherit: true
+    }
+};
