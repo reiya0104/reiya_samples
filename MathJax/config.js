c0861446f2a4558b01a32d9433906e4a91d521b4
Reiya <reiya0104@ezweb.ne.jp>
2020-06-15 00:30:40 +0900

    Update config.js

--- MathJax/config.js
+++ MathJax/config.js
@@ -1,7 +1,6 @@
 // This is a JavaScript file named "config.js"
-window.MathJax = {
-    TeX: {
-        equationNumbers: {autoNumber: "AMS"},
+MathJax = {
+    teX: {
         inlineMath: [
             ["$", "$"],
             ["\\(", "\\)"]
@@ -11,15 +10,11 @@ window.MathJax = {
             ['\\[', '\\]']
         ],
         processEscapes: true,
-        Macros: {
+        macros: {
             x: "{\\times}",
             bm: ["{\\boldsymbol{#1}}",1],
             dd: ["{\\frac{\\partial #1}{\\partial #2}}",2],
             mat: ["{\\begin{pmatrix} #1 & #2 \\\\ #3 & #4 \\end{pmatrix}}",4]
         }
-    },
-    CommonHTML: {
-        scale: 90,
-        mtextFontInherit: true
     }
 };
